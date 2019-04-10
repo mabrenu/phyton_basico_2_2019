1d3850848e1a884dcbffb386e4d6944fefc14038
mabrenu <49101176+mabrenu@users.noreply.github.com>
2019-04-10 04:41:41 -0600

    Tareas Semanales

---
+++ Tareas/Tarea_1.py
@@ -0,0 +1,10 @@
+#* 3.1415926 ** 3.141592 *** 3.14159 **** 3.1415 ***** 3.141 ****** 3.14
+
+stringValue = "*****************"
+floatValue = 3.1415926
+
+print("%.1s %.7f %.2s %.6f %.3s %.5f %.4s %.4f %.5s %.3f %.6s %.2f"
+      % (stringValue, floatValue, stringValue, floatValue, stringValue, floatValue,
+         stringValue, floatValue, stringValue, floatValue, stringValue, floatValue))
+
+
